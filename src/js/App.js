59efba6b8534971b19894e7c9b251f3a69277c7b
theart84 <artshadow74@gmail.com>
2021-03-11 23:36:03 +0500

    fixed bugs

--- src/js/App.js
+++ src/js/App.js
@@ -1,7 +1,7 @@
 import Team from './Team';
 
 const newTeam = new Team();
-for (const newTeamElement of newTeam) {
+for (const newTeamElement of newTeam.teams) {
   console.log(newTeamElement);
 }
 console.log(newTeam);
--- src/js/Team.js
+++ src/js/Team.js
@@ -1,58 +1,58 @@
 export default class Team {
   constructor() {
-    this.swordsman = {
-      name: 'Сэр_Ланцелот',
-      type: 'Swordsman',
-      health: 70,
-      level: 1,
-      attack: 40,
-      defence: 10,
-    };
+    this.teams = [
+      {
+        name: 'Сэр_Ланцелот',
+        type: 'Swordsman',
+        health: 70,
+        level: 1,
+        attack: 40,
+        defence: 10,
+      },
 
-    this.bowman = {
-      name: 'Логоваз',
-      type: 'Bowman',
-      health: 50,
-      level: 1,
-      attack: 40,
-      defence: 10,
-    };
-    this.magician = {
-      name: 'Гендальф',
-      type: 'Magician',
-      health: 60,
-      level: 3,
-      attack: 40,
-      defence: 10,
-    };
-    this.daemon = {
-      name: 'xxxTrueNagibXxx',
-      type: 'Daemon',
-      health: 90,
-      level: 5,
-      attack: 50,
-      defence: 10,
-    };
+      {
+        name: 'Логоваз',
+        type: 'Bowman',
+        health: 50,
+        level: 1,
+        attack: 40,
+        defence: 10,
+      },
+      {
+        name: 'Гендальф',
+        type: 'Magician',
+        health: 60,
+        level: 3,
+        attack: 40,
+        defence: 10,
+      },
+      {
+        name: 'xxxTrueNagibXxx',
+        type: 'Daemon',
+        health: 90,
+        level: 5,
+        attack: 50,
+        defence: 10,
+      },
+    ];
   }
 
   [Symbol.iterator]() {
-    return this;
-  }
-
-  next() {
-    if (!this.current) {
-      this.current = 0;
-    }
-    if (this.current < Object.keys(this).length - 1) {
-      return {
-        done: false,
-        value: Object.values(this)[this.current],
-        current: ++this.current,
-      };
-    }
-    delete this.current;
     return {
-      done: true,
+      next() {
+        let current = 0;
+        const { teams } = this;
+        return {
+          next() {
+            const res = {
+              done: current >= teams.length,
+              value: teams[current],
+            };
+            current++;
+            return res;
+          },
+        };
+      },
     };
   }
 }
